32438c216c68be7552181ffdd82e6f285b3bd554
Victoria Jury <victoriaehodges@gmail.com>
2024-09-19 17:33:13 +0200

    add image to timer, adjust component positions and add borders

--- src/gui.py
+++ src/gui.py
@@ -1,4 +1,6 @@
-from tkinter import ttk
+import os
+import pathlib
+from tkinter import PhotoImage, ttk
 
 """
 Handles all the user interface components like buttons, sliders,
@@ -6,36 +8,51 @@ task checklists, and component layouts
 """
 
 
+def get_image_from_resources(img_file_name):
+    images_dir = pathlib.Path("resources/images/").resolve()
+    img_path = os.path.join(images_dir, img_file_name)
+    return img_path
+
+
 class TomatoTimer(ttk.Frame):
     def __init__(self, parent):
         ttk.Frame.__init__(self, parent)
 
-        label1 = ttk.Label(self, text="Timer")
-        label1.grid(row=0, column=0, columnspan=2)
+        self.bg_image = PhotoImage(file=get_image_from_resources("tomato_timer_bg.png"))
+
+        timer = ttk.Label(
+            self,
+            text="Timer",
+            image=self.bg_image,
+            compound="center",
+            padding=10,
+        )
+        timer.grid(row=0, column=0, columnspan=2)
 
         button_reset = ttk.Button(self, text="Reset")
-        button_reset.grid(row=1, column=0)
+        button_reset.grid(row=1, column=0, sticky="e", padx=5)
 
         button_start = ttk.Button(self, text="Start")
-        button_start.grid(row=1, column=1)
+        button_start.grid(row=1, column=1, sticky="w", padx=5)
 
 
 class TaskList(ttk.Frame):
     def __init__(self, parent):
         ttk.Frame.__init__(self, parent)
+        self.configure(border=1, borderwidth=1, relief="sunken", padding=10)
 
         label1 = ttk.Label(self, text="Tasks")
-        label1.grid(row=0, column=0)
+        label1.pack(side="top")
 
         self.task_list = ttk.Frame(self, padding=15)
-        self.task_list.grid(row=1, column=0)
+        self.task_list.pack(side="top")
 
-        ttk.Checkbutton(self.task_list, text="List item...").grid(row=0, column=0)
-        ttk.Checkbutton(self.task_list, text="List item...").grid(row=1, column=0)
-        ttk.Checkbutton(self.task_list, text="List item...").grid(row=2, column=0)
+        ttk.Checkbutton(self.task_list, text="List item...").pack()
+        ttk.Checkbutton(self.task_list, text="List item...").pack()
+        ttk.Checkbutton(self.task_list, text="List item...").pack()
 
         button_add_task = ttk.Button(self, text="Add new task")
-        button_add_task.grid(row=2, column=0)
+        button_add_task.pack(side="bottom")
 
 
 class SettingSlider(ttk.Frame):
