b5f111aa73cf099d4a41c0e9c8264c2ec59b3550
Victoria Jury <victoriaehodges@gmail.com>
2024-09-26 17:12:13 +0200

    add documentation

--- python_pomodoro/tasklist.py
+++ python_pomodoro/tasklist.py
@@ -3,6 +3,11 @@ from tkinter import Checkbutton, IntVar, Tk, ttk
 from typing import Optional
 from uuid import UUID, uuid4
 
+"""
+Handles creation of tasks in tasklist.
+New tasks are created as checkboxes and when completed/checked are coloured grey.
+"""
+
 
 @dataclass
 class Task:
@@ -25,7 +30,7 @@ class TaskList(ttk.Frame):
         self.task_list = ttk.Frame(self, padding=5)
         self.task_list.pack(side="top")
 
-        # Creat some test tasks:
+        # Create some test tasks:
         task1_id = uuid4()
         self.tasks_by_id: dict[UUID, Task] = {task1_id: Task(id=task1_id, title="Some task")}
         for _, task in self.tasks_by_id.items():
@@ -43,15 +48,19 @@ class TaskList(ttk.Frame):
         self.button_save_task.pack(side="bottom")
         self.entry_task_input.pack(side="bottom", pady=10)
         self.entry_task_input.focus()
+        # Press enter key to save task
         self.main_window.bind("<Return>", self.save_new_task)
 
     def save_new_task(self, event=None) -> None:
         self.button_save_task.pack_forget()
         self.entry_task_input.pack_forget()
         self.button_add_task.pack(side="bottom")
+
         task = Task(uuid4(), self.entry_task_input.get())
         if task.title:
             self.create_task_checkbutton(task)
+
+        # Prevent new task creation with enter key and clear input
         self.main_window.unbind("<Return>")
         self.entry_task_input.delete(0, "end")
         self.focus()
